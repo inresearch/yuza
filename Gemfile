dbbbf83bc8559e7eaebc9861db3bb9537436938b
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-11-05 14:05:49 +0700

    signin form and foundational style

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'rack-cors'
 gem 'slim'
+gem 'activeadmin'
+gem 'bootstrap', '4.0.0.beta2.1'
+gem 'jquery-rails'
 
 group :development, :test do
   gem 'pry-byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/signin'
-
-  get 'users/signup'
+  resources :users, only: [] do
+    collection do
+      get 'signin'
+      get 'signup'
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
